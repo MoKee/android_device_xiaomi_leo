1557fd3605103900f6eaf5195966ba58611db161
GuaiYiHu <guaiyihu@foxmail.com>
2017-02-26 08:28:39 +0800

    leo:fix build error

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -9,6 +9,9 @@ ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 # include the non-open-source counterpart to this file
 -include vendor/xiaomi/leo/AndroidBoardVendor.mk
 
+#Work areound for the failure of making KERNEL_OBJ/usr directory 
+$(shell mkdir -p $(OUT)/obj/KERNEL_OBJ/usr)
+
 # Create symbolic links
 $(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wlan/qca_cld; \
         ln -sf  /system/etc/wifi/WCNSS_qcom_wlan_nv.bin \
